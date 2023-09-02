a3d15a221b02e72a44522f2709e4a74632173738
Umole Andrew <39883080+umole@users.noreply.github.com>
2023-09-03 00:52:36 +0100

    Sound and background image added

--- index.js
+++ index.js
@@ -5,6 +5,40 @@ for (const myButton of myButtons) {
 }
 
 function playAudio() {
-    const audio = new Audio('./sounds/crash.mp3');
-    audio.play();
+    // const audio = new Audio('./sounds/crash.mp3');
+    // audio.play();
+
+    switch (this.innerHTML) {
+        case 'w':
+            const audio1 = new Audio('./sounds/crash.mp3');
+            audio1.play();
+            break;
+        case 'a':
+            const audio2 = new Audio('./sounds/kick-bass.mp3');
+            audio2.play();
+            break;
+        case 's':
+            const audio3 = new Audio('./sounds/snare.mp3');
+            audio3.play();
+            break;
+        case 'd':
+            const audio4 = new Audio('./sounds/tom-1.mp3');
+            audio4.play();
+            break;
+        case 'j':
+            const audio5 = new Audio('./sounds/tom-2.mp3');
+            audio5.play();
+            break;
+        case 'k':
+            const audio6 = new Audio('./sounds/tom-3.mp3');
+            audio6.play();
+            break;
+        case 'l':
+            const audio7 = new Audio('./sounds/tom-4.mp3');
+            audio7.play();
+            break;
+    
+        default:
+            break;
+    }
 }
